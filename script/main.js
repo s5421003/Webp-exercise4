d91859f8a08e1eb122fe7a09ef9da6a419fe88b0
s5421003 <114901226+s5421003@users.noreply.github.com>
2022-10-24 15:56:12 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -8,9 +8,31 @@ function randomValueFromArray(array){
 }
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-
 let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-
 let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-
 let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(0.0714) + ' stone';
+    const temperature =  Math.round(34.444) + ' centigrade';
+    
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
+
+let newStory = storyText;
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+newStory.replace(xItem, yItem, zItem);
