9cf075dbdb44a91dcc7816be7bb268a54aae028b
s5421003 <114901226+s5421003@users.noreply.github.com>
2022-10-24 15:20:12 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -7,16 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 
-the soup kitchen
-Disneyland
-the White House
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
